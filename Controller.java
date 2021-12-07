7c9fb30fcaaa646704462b7700f2ab90340a30a2
dongkyu <dongkyu.parkk@gmail.com>
2021-12-07 21:45:55 +0900

    refactor: refactoring Controller.java
    separate Controller constructor fucntion

--- Controller.java
+++ Controller.java
@@ -7,101 +7,7 @@ public class Controller {
     public Controller(Map map, Character control) {
         this.map = map;
         this.control = control;
-
-        String[] arr = this.map.getMapInfo().getPlayerPosition().split(", ");
-
-        int xPosition = Integer.parseInt(arr[0].substring(1)) - 1;
-        int yPosition = Integer.parseInt(arr[1].substring(0, arr[1].length() - 1)) - 1;
-
-        if (this.control == 'w') {
-            if (this.map.getMap()[xPosition - 1][yPosition] == -1) {
-                this.map.getMap()[xPosition - 1][yPosition] = 3;
-                if (this.map.getMap()[xPosition][yPosition] == 6) {
-                    this.map.getMap()[xPosition][yPosition] = 1;
-                } else {
-                    this.map.getMap()[xPosition][yPosition] = -1;
-                }
-                this.map.getMapInfo().setPlayerPosition("(" + xPosition + ", " + (yPosition + 1) + ")");
-                this.success = true;
-            } else if (this.map.getMap()[xPosition - 1][yPosition] == 1) {
-                this.map.getMap()[xPosition - 1][yPosition] = 6;
-                if (this.map.getMap()[xPosition][yPosition] == 6) {
-                    this.map.getMap()[xPosition][yPosition] = 1;
-                } else {
-                    this.map.getMap()[xPosition][yPosition] = -1;
-                }
-                this.map.getMapInfo().setPlayerPosition("(" + xPosition + ", " + (yPosition + 1) + ")");
-                this.success = true;
-            } else {
-                this.success = false;
-            }
-        } else if (this.control == 'a') {
-            if (this.map.getMap()[xPosition][yPosition - 1] == -1) {
-                this.map.getMap()[xPosition][yPosition - 1] = 3;
-                if (this.map.getMap()[xPosition][yPosition] == 6) {
-                    this.map.getMap()[xPosition][yPosition] = 1;
-                } else {
-                    this.map.getMap()[xPosition][yPosition] = -1;
-                }
-                this.map.getMapInfo().setPlayerPosition("(" + (xPosition + 1) + ", " + yPosition + ")");
-                this.success = true;
-            } else if (this.map.getMap()[xPosition][yPosition - 1] == 1) {
-                this.map.getMap()[xPosition][yPosition - 1] = 6;
-                if (this.map.getMap()[xPosition][yPosition] == 6) {
-                    this.map.getMap()[xPosition][yPosition] = 1;
-                } else {
-                    this.map.getMap()[xPosition][yPosition] = -1;
-                }
-                this.map.getMapInfo().setPlayerPosition("(" + (xPosition + 1) + ", " + yPosition + ")");
-                this.success = true;
-            } else {
-                this.success = false;
-            }
-        } else if (this.control == 's') {
-            if (this.map.getMap()[xPosition + 1][yPosition] == -1) {
-                this.map.getMap()[xPosition + 1][yPosition] = 3;
-                if (this.map.getMap()[xPosition][yPosition] == 6) {
-                    this.map.getMap()[xPosition][yPosition] = 1;
-                } else {
-                    this.map.getMap()[xPosition][yPosition] = -1;
-                }
-                this.map.getMapInfo().setPlayerPosition("(" + (xPosition + 2) + ", " + (yPosition + 1) + ")");
-                this.success = true;
-            } else if (this.map.getMap()[xPosition + 1][yPosition] == 1) {
-                this.map.getMap()[xPosition + 1][yPosition] = 6;
-                if (this.map.getMap()[xPosition][yPosition] == 6) {
-                    this.map.getMap()[xPosition][yPosition] = 1;
-                } else {
-                    this.map.getMap()[xPosition][yPosition] = -1;
-                }
-                this.map.getMapInfo().setPlayerPosition("(" + (xPosition + 2) + ", " + (yPosition + 1) + ")");
-                this.success = true;
-            } else {
-                this.success = false;
-            }
-        } else if (this.control == 'd') {
-            if (this.map.getMap()[xPosition][yPosition + 1] == -1) {
-                this.map.getMap()[xPosition][yPosition + 1] = 3;
-                if (this.map.getMap()[xPosition][yPosition] == 6) {
-                    this.map.getMap()[xPosition][yPosition] = 1;
-                } else {
-                    this.map.getMap()[xPosition][yPosition] = -1;
-                }
-                this.map.getMapInfo().setPlayerPosition("(" + (xPosition + 1) + ", " + (yPosition + 2) + ")");
-                this.success = true;
-            } else if (this.map.getMap()[xPosition][yPosition + 1] == 1) {
-                this.map.getMap()[xPosition][yPosition + 1] = 6;
-                if (this.map.getMap()[xPosition][yPosition] == 6) {
-                    this.map.getMap()[xPosition][yPosition] = 1;
-                } else {
-                    this.map.getMap()[xPosition][yPosition] = -1;
-                }
-                this.map.getMapInfo().setPlayerPosition("(" + (xPosition + 1) + ", " + (yPosition + 2) + ")");
-                this.success = true;
-            } else {
-                this.success = false;
-            }
-        }
+        controlPad(this.control);
     }
 
     public Map getMap() {
@@ -110,5 +16,78 @@ public class Controller {
 
     public boolean isSuccess() {
         return success;
+    }
+
+    public void controlPad(Character control) {
+        String[] arr = this.map.getMapInfo().getPlayerPosition().split(", ");
+
+        int xPosition = Integer.parseInt(arr[0].substring(1)) - 1;
+        int yPosition = Integer.parseInt(arr[1].substring(0, arr[1].length() - 1)) - 1;
+
+        if (control == 'w') {
+            if (setPlayerMoveSpace(xPosition - 1, yPosition)) {
+                setAfterPlayerMove(xPosition, yPosition);
+                this.map.getMapInfo().setPlayerPosition("(" + xPosition + ", " + (yPosition + 1) + ")");
+                this.success = true;
+                return;
+            }
+            this.success = false;
+            return;
+        }
+
+        if (control == 'a') {
+            if (setPlayerMoveSpace(xPosition, yPosition - 1)) {
+                setAfterPlayerMove(xPosition, yPosition);
+                this.map.getMapInfo().setPlayerPosition("(" + (xPosition + 1) + ", " + yPosition + ")");
+                this.success = true;
+                return;
+            }
+            this.success = false;
+            return;
+        }
+
+        if (control == 's') {
+            if (setPlayerMoveSpace(xPosition + 1, yPosition)) {
+                setAfterPlayerMove(xPosition, yPosition);
+                this.map.getMapInfo().setPlayerPosition("(" + (xPosition + 2) + ", " + (yPosition + 1) + ")");
+                this.success = true;
+                return;
+            }
+            this.success = false;
+            return;
+        }
+
+        if (control == 'd') {
+            if (setPlayerMoveSpace(xPosition, yPosition + 1)) {
+                setAfterPlayerMove(xPosition, yPosition);
+                this.map.getMapInfo().setPlayerPosition("(" + (xPosition + 1) + ", " + (yPosition + 2) + ")");
+                this.success = true;
+                return;
+            }
+            this.success = false;
+            return;
+        }
+    }
+
+    public boolean setPlayerMoveSpace(int xPosition, int yPosition) {
+        if (this.map.getMap()[xPosition][yPosition] == -1) { // destination is Empty
+            this.map.getMap()[xPosition][yPosition] = 3;
+            this.map.getMapInfo().setPlayerPosition("(" + xPosition + ", " + (yPosition + 1) + ")");
+            return true;
+        }
+        if (this.map.getMap()[xPosition][yPosition] == 1) { // destination is hall
+            this.map.getMap()[xPosition][yPosition] = 6;
+            this.map.getMapInfo().setPlayerPosition("(" + xPosition + ", " + (yPosition + 1) + ")");
+            return true;
+        }
+        return false;
+    }
+
+    public void setAfterPlayerMove(int xPosition, int yPosition) {
+        if (this.map.getMap()[xPosition][yPosition] == 6) { // player in hall
+            this.map.getMap()[xPosition][yPosition] = 1;
+        } else { // not in hall
+            this.map.getMap()[xPosition][yPosition] = -1;
+        }
     }
 }
